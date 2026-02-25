d7a92abb8f45404584bcce97299731b38c860e90
Jettberg <jettmberg@gmail.com>
2026-02-24 18:52:05 -0600

    inial commit for all user auth deliverables. Created skeleton - pre testing / fixing

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -119,3 +119,7 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/recipes/"
+LOGOUT_REDIRECT_URL = "/success/"
--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Recipe
 
 
@@ -6,17 +7,22 @@ def home(request):
     return render(request, "recipes/recipes_home.html")
 
 
+@login_required
 def recipe_list(request):
     recipes = Recipe.objects.all().order_by("-created_at")
     return render(request, "recipes/recipes_list.html", {"recipes": recipes})
 
 
+@login_required
 def recipe_detail(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
-    # difficulty is already stored, but we can also display calculated difficulty for safety
     calculated_difficulty = recipe.calculate_difficulty()
     return render(
         request,
         "recipes/recipe_detail.html",
         {"recipe": recipe, "calculated_difficulty": calculated_difficulty},
     )
+
+
+def logout_success(request):
+    return render(request, "recipes/success.html")
